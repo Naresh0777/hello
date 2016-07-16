b23893e22ef52464681c3b2ee9fa793a258e5139
Naresh0777 <hariharannaresh07@gmail.com>
2016-07-16 15:13:17 +0530

    Add files via upload

---
+++ stoi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package area;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class stoi {
+    public static void main(String[] args) {
+        String s1;
+        Scanner s=new Scanner(System.in);
+        s1=s.nextLine();
+        int n= Integer.parseInt(s1);
+        System.out.println(n);
+    }
+    
+}
