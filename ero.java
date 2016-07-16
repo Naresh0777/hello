87525ff34e9f5acf469cd6d9b7410583d9653721
Naresh0777 <hariharannaresh07@gmail.com>
2016-07-16 15:49:43 +0530

    Add files via upload

---
+++ ero.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package area;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class ero {
+    public static void main(String[] args) {
+        int n;
+         Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        if(n%2==0){
+            System.out.println("even number");
+        }
+        else
+            System.out.println("odd number");
+            
+        }
+    }
+    
+
